cf0da2cf97c49b3d598347abd3b23a2f995fa251
Lenington Alfredo Del Orbe Serrano <lenington_delorbe@ucne.edu.do>
2018-05-25 23:54:22 -0400

    Clase Contexto terminada.

---
+++ ArticulosRegistro/DAL/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using ArticulosRegistro.Entidades;
+
+namespace ArticulosRegistro.DAL
+{
+    //Aqui agregamos public también, para que la clase pueda ser encontradaen cualquier parte del proyecto
+    //y heredamos de DbContext para que EntityFramework haga su trabajo
+    public class Contexto : DbContext
+    {
+        public DbSet<Articulo> articulo { get; set; }
+
+
+        //base("ConStr") para pasar la conexion a EntityFramework
+        public Contexto() : base("ConStr")
+        { }
+    }
+}
